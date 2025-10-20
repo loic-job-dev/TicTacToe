dfa559e5cf2ea2431b34c93a0e254043b661ebc2
loic jobard <loic-jobard@le-campus-numerique.fr>
2025-10-20 13:42:20 +0200

    begining the Connect4 class

---
+++ src/main/java/fr/campus/loic/tictactoe/logic/Connect4.java
@@ -0,0 +1,14 @@
+package fr.campus.loic.tictactoe.logic;
+
+/**
+ * Handles the logic of a Connect4 game, including player moves and board display.
+ */
+public class Connect4 extends Game{
+
+    /** Creates a new Gomoku game with a 15x15 board. */
+    public Connect4() {
+        super(6, 7, 4);
+    }
+
+
+}
--- src/main/java/fr/campus/loic/tictactoe/logic/Gomoku.java
+++ src/main/java/fr/campus/loic/tictactoe/logic/Gomoku.java
@@ -14,41 +14,8 @@ public class Gomoku extends Game {
 
     //private Player[] players;
 
-    /** Creates a new Tic-Tac-Toe game with a 3x3 board. */
+    /** Creates a new Gomoku game with a 15x15 board. */
     public Gomoku() {
         super(15, 15, 5);
-    }
-
-    @Override
-    public void chooseGameMode() {
-        int choice = 0;
-
-        while (true) {
-            try {
-                view.println(ConsoleColors.PURPLE + Fr.chooseGameMode + ConsoleColors.RESET);
-                choice = clavier.nextInt();
-
-                if (choice == 1) {
-                    this.player1 = new HumanPlayer("X", 3);
-                    this.player2 = new HumanPlayer("O", 4);
-                    break;
-                }
-                else if (choice == 2) {
-                    this.player1 = new HumanPlayer("X", 3);
-                    this.player2 = new ArtificialPlayer("O", 4);
-                    break;
-                }
-                else if (choice == 3) {
-                    this.player1 = new ArtificialPlayer("X", 3);
-                    this.player2 = new ArtificialPlayer("O", 4);
-                    break;
-                } else {
-                    view.println(ConsoleColors.RED + Fr.wrongChoice +  ConsoleColors.RESET);
-                }
-            } catch (InputMismatchException e) {
-                view.println(ConsoleColors.RED + Fr.exceptionIntMessage +  ConsoleColors.RESET);
-                clavier.nextLine();
-            }
-        }
     }
 }
